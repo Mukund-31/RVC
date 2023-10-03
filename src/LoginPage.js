43c3c12c7d600eca3ca8c21430e994f3f8ad6e53
mukund <mukundverma.cd22@rvce.edu.in>
2023-10-04 03:56:24 +0530

    e

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from 'react';
 // import axios from 'axios';
 import rvclogo from './rvclogo.png';
-function LoginPage({ setIsAuthenticated }) {
+import CrossIcon from "./cross.png";
+function LoginPage({user, setIsAuthenticated }) {
+    const [showuserinfoForm, setShowuserinfoForm] = useState(false);
+    const [profilePic, setProfilePic] = useState();
+    const [newName, setNewName] = useState();
+    const [newBio, setNewBio] = useState()
     const [formData, setFormData] = useState({
         username: '',
         password: '',
@@ -36,6 +41,7 @@ function LoginPage({ setIsAuthenticated }) {
 
     const handleTabChange = (isSignup) => {
         setIsSignup(isSignup);
+        setShowuserinfoForm(false);
     };
 
 
@@ -105,13 +111,17 @@ function LoginPage({ setIsAuthenticated }) {
 
             // Send a POST request to the authentication endpoint for signup...
             // Update the authentication status
-            setIsAuthenticated(true);
+            setIsAuthenticated(false);
             // Clear error message
             setError('');
+
         } catch (error) {
             // Handle errors here...
         }
+
     };
+
+
 
     const shadowAnimation = {
 
@@ -140,6 +150,34 @@ function LoginPage({ setIsAuthenticated }) {
 }`;
 
 
+    const handleSignupClick = () => {
+        setShowuserinfoForm(true);
+
+    };
+
+
+    const handleProfilePicChange = (e) => {
+        const file = e.target.files[0];
+        // You can add code here to upload the selected profile picture to a server or display it on the page.
+        setProfilePic(file);
+    };
+
+    const handleNameChange = (e) => {
+        setNewName(e.target.value);
+    };
+
+    const handleBioChange = (e) => {
+        setNewBio(e.target.value);
+    };
+
+    const handleUserInfoSubmit = (e) => {
+        e.preventDefault();
+        // Add code to submit the updated profile information
+        // You can include an API call or any other logic to save the user's profile data.
+        setIsAuthenticated(true);
+
+    };
+
     return (
         <div style={{backgroundColor:'#fff',display: 'flex', flexDirection: 'column', justifyContent:'center', alignItems: 'center' }} >
             <style>{gradientShadowAnimation}</style>
@@ -151,7 +189,7 @@ function LoginPage({ setIsAuthenticated }) {
             </div>
 
             {isSignup ? (
-                <form onSubmit={handleSignupSubmit}   style={{   paddingTop:'15px', height:'350px', width: '85%', maxWidth: '400px', textAlign: 'center', padding: '0 20px', position: 'relative',  borderRadius: '11px', ...shadowAnimation,  }}>
+                <form onSubmit={handleSignupSubmit} style={{   paddingTop:'15px', height:'350px', width: '85%', maxWidth: '400px', textAlign: 'center', padding: '0 20px', position: 'relative',  borderRadius: '11px', ...shadowAnimation,  }}>
 
                     <div>
                         <input
@@ -278,13 +316,14 @@ function LoginPage({ setIsAuthenticated }) {
 
                     </div>
 
-
-                    <button type="submit" style={{
+                    <button onClick={handleSignupClick} type="submit" style={{
                         boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.9)',color:'#fff', fontFamily: 'Helvetica', width: 'calc(100% - 25px)', height: '40px',background:'#000',border:'1px solid #ccc',fontSize:'18px',borderRadius: '50px',
-                    }}>
+                    }}  >
                         Sign Up
                     </button>
                 </form>
+
+
             ) : (
 
             <form  onSubmit={handleLoginSubmit} style={{...shadowAnimation, marginTop:'50px', paddingTop:'16px', height:'200px', width: '85%', maxWidth: '400px', textAlign: 'center', padding: '0 20px', position: 'relative', background: 'transparent', borderRadius: '11px',  }}>
@@ -334,8 +373,90 @@ function LoginPage({ setIsAuthenticated }) {
                 >Log In</button>
             </form>
             )}
-
             {error && <div style={{ color: 'red' }}>{error}</div>}
+
+            {showuserinfoForm && (
+                <form  onSubmit={handleUserInfoSubmit} style={{  overflowY:'scroll',position: 'fixed', bottom: -1, left: 0, height:'99%',width: '100%', backgroundColor: 'white',  zIndex: '100',borderTopRightRadius:'20px',borderTopLeftRadius:'20px', border:'0px solid #000',boxShadow: '0px 3px 9px rgba(0, 0, 0, 1)' }}>
+
+                    <input
+                        type="file"
+                        id="fileInput"
+                        accept="image/*"
+                        onChange={handleProfilePicChange}
+                        style={{
+                            display: 'none',
+
+                        }}
+                    />
+                    <label htmlFor="fileInput">
+                        <img
+                            src={profilePic}
+                            style={{
+                                boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.9)',
+                                marginBottom: '15px',
+                                fontFamily: 'Helvetica',
+                                width: '150px',
+                                height: '150px',
+                                background: 'rgba(255, 252, 255, 0.5)',
+                                border: '1px solid #ccc',
+                                fontSize: '10px',
+                                zIndex: '1',
+                                borderRadius: '50%',
+                                position:'absolute',
+                                left: '50%',
+                                top: '15%',
+                                transform: 'translate(-50%, -50%)'
+                            }}
+                        />
+                    </label>
+                    <input
+                        type="text"
+                        placeholder="Name"
+                        value={newName}
+                        onChange={handleNameChange}
+                        style={{boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.9)',
+                            marginBottom: '15px',
+                            position:'absolute',
+                            left: '50%',
+                            top: '30%',
+                            transform: 'translate(-50%, -50%)',
+                            paddingLeft: '18px',
+                            fontFamily: 'Helvetica',
+                            width: 'calc(90% - 25px)',
+                            height: '40px',
+                            background: 'rgba(255, 255, 255, 0.5)',
+                            border: '1px solid #ccc',
+                            fontSize: '18px',
+                            zIndex: '1',
+                            borderRadius: '11px',}}
+                    />
+                    <textarea
+                        placeholder="Bio"
+                        value={newBio}
+                        onChange={handleBioChange}
+                        style={{boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.9)',
+                            marginBottom: '15px',
+                            paddingLeft: '18px',
+                            position:'absolute',
+                            left: '50%',
+                            top: '41%',
+                            transform: 'translate(-50%, -50%)',
+                            fontFamily: 'Helvetica',
+                            width: 'calc(90% - 25px)',
+                            height: '100px',
+                            background: 'rgba(255, 255, 255, 0.5)',
+                            border: '1px solid #ccc',
+                            fontSize: '18px',
+                            zIndex: '1',
+                            borderRadius: '11px',}}
+                    />
+                    {/* Include the code to submit the updated profile information */}
+                    <button type="submit"
+                            style={{  position:'absolute', left: '50%', top: '52%',
+                                transform: 'translate(-50%, -50%)',boxShadow: '0px 3px 6px rgba(0, 0, 0, 0.9)',color:'#fff', fontFamily: 'Helvetica', width: '100px', height: '40px',background:'#000',border:'1px solid #ccc',fontSize:'18px',borderRadius: '11px',}}
+                            >Submit</button>
+                </form>
+            )}
 
         </div>
 
